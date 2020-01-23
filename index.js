ceaa3abadd1b88f68fb152c8defaef8efda76050
Davia DeNisco <davia622@gmail.com>
2020-01-23 13:03:52 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let canvas = document.getElementById('game-screen');
+let ctx = canvas.getContext('2d');
+
+ctx.clearRect(0, 0, 800, 600);
+
+ctx.fillStyle = '#f00';
+ctx.fillRect(20, 20, 100, 100);
+ctx.fillStyle = '#00f';
+ctx.fillRect(200, 200, 50, 50);
+
